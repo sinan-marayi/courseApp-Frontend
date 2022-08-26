d91137683074f2592988c38f15fbaa000b6f8416
sinan-marayi <sinanmarayi8@gmail.com>
2022-08-26 14:42:10 +0530

    backend updated

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -8,20 +8,20 @@ export class ApiService {
   constructor(private http: HttpClient) {}
 
   getCourse = () => {
-    return this.http.get('http://localhost:3000/api/view');
+    return this.http.get('http://52.1.249.26:3000/api/view');
   };
 
 
   addCourse = (data: any) => {
-    return this.http.post('http://localhost:3000/api/add', data);
+    return this.http.post('http://52.1.249.26:3000/api/add', data);
   };
 
 
   removeCourse = (data: any) => {
-    return this.http.post('http://localhost:3000/api/deleteById', data);
+    return this.http.post('http://52.1.249.26:3000/api/deleteById', data);
   };
 
   searchCourse=(data:any)=>{
-    return this.http.post('http://localhost:3000/api/search',data)
+    return this.http.post('http://52.1.249.26:3000/api/search',data)
   }
 }
